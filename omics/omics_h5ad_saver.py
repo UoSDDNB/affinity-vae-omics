c2957755622e790ab84beae469e1254fb63e0d3b
moi-taiga <moi.t.nicholas@gmail.com>
2025-10-23 14:46:06 +0100

    Scrap masking options, simplify output path's as user inputs. comment out nHVG selection.

--- omics/omics_h5ad_saver.py
+++ omics/omics_h5ad_saver.py
@@ -1,7 +1,7 @@
 # Importing the neccecary modules
 import argparse
 import anndata
-import scanpy
+#import scanpy
 import os
 import numpy
 import scipy.sparse as sp
@@ -10,7 +10,7 @@ import scipy.sparse as sp
 # append class (cell_type) label and save as individual files.
 
 
-def matrix_split_save(sc_matrix, cell_types, output_path):
+def matrix_split_save(sc_matrix, cell_types, input_arrays_path):
     """
         Split obs (cells) from a sparce matrix (extracted from a .h5ad file)
         and save into individual .npy files.
@@ -21,12 +21,12 @@ def matrix_split_save(sc_matrix, cell_types, output_path):
             Sparce matrix data (from a .h5ad file) to be split and saved.
         cell_types : list
             A List containing class labels (cell type) for each sample (cell).
-        output_path : str
+        input_arrays_path : str
             Path to the directory where the data will be saved.
 
         Returns:
         --------
-        None (files are saved to output_path directory)
+        None (files are saved to input_arrays_path directory)
 
         Notes:
         ------
@@ -41,8 +41,8 @@ def matrix_split_save(sc_matrix, cell_types, output_path):
         --------
         sc_matrix = ...  # Load your omic matrix data
         cell_types = ['Class1', 'Class2', 'Class1', ...]  # Class labels for each sample
-        output_directory = '/path/to/output'  # Directory where the data will be saved
-        matrix_split_save(sc_matrix, cell_types, output_directory)
+        input_arrays_path = '/path/to/output'  # Directory where the data will be saved
+        matrix_split_save(sc_matrix, cell_types, input_arrays_path)
         """
 
     # Create a list to store individual arrays
@@ -59,16 +59,14 @@ def matrix_split_save(sc_matrix, cell_types, output_path):
     cell_array_lst = sc_matrix.tolist()  # Optimized version of above, encounters issues with lack of memory.
     
 
-    # Make sure that the user has a directory named input_arrays.
-    # if not, create it.
-    input_arrays_path = os.path.join(output_path, f'input_arrays{suffix}')
+    # Make sure that the user has a directory named 'input_arrays_path'.
+    # if it exists, stop with error
     if os.path.exists(input_arrays_path):
-        print(f"Warning '{input_arrays_path}' directory already exists")
-        input_arrays_warning_printed = True
+        print(f"Error: '{input_arrays_path}' directory already exists. Please specify a different path or remove the directory.")
+        exit(1)
     else:
         os.makedirs(input_arrays_path)
         print(f"Directory '{input_arrays_path}' created.")
-        input_arrays_warning_printed = False
 
     # save each array with a file name prefixed with its class label
     #idx = 0
@@ -79,27 +77,23 @@ def matrix_split_save(sc_matrix, cell_types, output_path):
         # print(sample.shape)
 
         # Build save path
-        save_path = os.path.join(output_path, f'input_arrays{suffix}', sample_name)
-
+        save_path = os.path.join(input_arrays_path, sample_name)
         # save the sample arrays
         numpy.save(save_path, sample)
+        # if idx is a multiple of quater of the total loop itteratons, print a message to indicate progress
         idx = idx + 1
         # if idx is a multiple of quater of the total loop itteratons, print a message to indicate progress
         if idx % (len(cell_names) // 4) == 0:
             print(f"Saved sample {idx}/{len(cell_names)}")
 
-    # check that the number of files in input_arrays is equal to the number of saved samples
-    if idx != len(os.listdir(input_arrays_path)):
-        print("Warning: Number of files saved does not match the number of samples in input_arrays")
+    # check that the number of files in input_arrays_path is equal to the number of saved samples
+    if idx == len(os.listdir(input_arrays_path)):
+        print(f"all {idx} samples saved successfully.")
 
-    # Warning message at the end to indicate data may have been overwritten or appended to input_arrays,
-    # (only if the first one was printed).
-    if input_arrays_warning_printed:
-        print(f"Warning: Samples added to previously generated directory '{input_arrays_path}'.")
+
+
 
 # parse arguments to extract file paths for saving down the data
-
-
 if __name__ == '__main__':
 
     # Creating an ArgumentParser object
@@ -113,10 +107,18 @@ if __name__ == '__main__':
             "(includes counts matrix and cell_type column in the metadata)"
     )
 
-    # Argument for the output path to save data
+    # Argument for the output path to save data (input_arrays)
     parser.add_argument(
-        "--output_path",
-        help="Output path to save data"
+        "--input_arrays_path",
+        required=True,
+        help="Path to save individual sample arrays (.npy files)"
+    )
+
+    # Argument for the class list CSV file
+    parser.add_argument(
+        "--class_lst_path",
+        required=True,
+        help="Path to save the unique class list CSV file"
     )
 
     # Argument for the col name in the metadata that contains the cell-type info
@@ -127,44 +129,24 @@ if __name__ == '__main__':
         "Default is 'cell_type'"
     )
 
-    # Argument for the number of most variable genes to include in the analysis
-    parser.add_argument(
-        "--ngenes",
-        default=0,
-        help="The integer of the most vaiable genes to be included, \n" \
-             "Default is '0' (all genes)"
-    )
-
-    # Argument for excluding specific cell types
-    # these changes have a bug in generation of class_lst.csv (missing/too many classes)
-    parser.add_argument(
-        "--exclude_celltypes",
-        default="",
-        help="Comma-separated list of cell types to exclude (e.g. T,B)"
-    )
-
-    # Argument for including only specific cell types
-    parser.add_argument(
-        "--include_only_celltypes",
-        default="",
-        help="Comma-separated list of cell types to include only (e.g. T,B)"
-    )
+    # # Argument for the number of most variable genes to include in the analysis
+    # parser.add_argument(
+    #     "--ngenes",
+    #     default=0,
+    #     help="The integer of the most vaiable genes to be included, \n" \
+    #          "Default is '0' (all genes)"
+    # )
 
     # Parsing the command-line arguments
     args = parser.parse_args()
-
     print("Parsing command-line arguments...")
-
 
     # Assigning values from parsed arguments to variables
     h5ad_file = args.h5ad_file
-    output_path = args.output_path
+    input_arrays_path = args.input_arrays_path
+    class_lst_path = args.class_lst_path
     cell_type_column_name = args.cell_type_column_name
-    ngenes = int(args.ngenes)
-
-    # Parse exclude/include celltypes
-    exclude_celltypes = [x for x in args.exclude_celltypes.split(",") if x]
-    include_only_celltypes = [x for x in args.include_only_celltypes.split(",") if x]
+    # ngenes = int(args.ngenes)
 
     print(f"Reading .h5ad file from: {h5ad_file}")
 
@@ -172,41 +154,18 @@ if __name__ == '__main__':
     adata = anndata.read_h5ad(h5ad_file)
     print(r"Read in the .h5ad file")
 
-    # make a table of counts of each cell type in the data
-    print(f"Cells per cell_type: \n \n {adata.obs[cell_type_column_name].value_counts()}")
-
-    # Filter by include_only_celltypes or exclude_celltypes
-    suffix = ""
-    if include_only_celltypes:
-        print(f"Including only cell types: {include_only_celltypes}")
-        adata = adata[adata.obs[cell_type_column_name].isin(include_only_celltypes)].copy()
-        suffix = "_ONLY_" + "_".join(include_only_celltypes)
-        suffix = suffix.replace(" ", "-")
-    elif exclude_celltypes:
-        print(f"Excluding cell types: {exclude_celltypes}")
-        adata = adata[~adata.obs[cell_type_column_name].isin(exclude_celltypes)].copy()
-        suffix = "_MASKED_" + "_".join(exclude_celltypes)
-        suffix = suffix.replace(" ", "-")
-    # else: output_path remains as given
-
-    # make a table of counts of each cell type in the data
-    print(f"Cells per cell_type post cell_type filter: \n \n {adata.obs[cell_type_column_name].value_counts()}")
-    
-
-
     # If --ngenes is > 0, subset the data to the top ngenes
-    if ngenes > 0:
-        print(f"Subsetting the data to the top {ngenes} most variable genes")
-        # identify the most variable genes
-        scanpy.pp.highly_variable_genes(adata, n_top_genes=ngenes, flavor='cell_ranger')
-        # this could be skipped by using subset = True above
-        # downsample to only include the most variable genes
-        adata = adata[:, adata.var.highly_variable]
-        print(f"Subsetted the data to the top {ngenes} highly variable genes")
-    else:
-        print("Using all genes (no subsetting).")
+    # if ngenes > 0:
+    #     print(f"Subsetting the data to the top {ngenes} most variable genes")
+    #     # identify the most variable genes
+    #     scanpy.pp.highly_variable_genes(adata, n_top_genes=ngenes, flavor='cell_ranger')
+    #     # this could be skipped by using subset = True above
+    #     # downsample to only include the most variable genes
+    #     adata = adata[:, adata.var.highly_variable]
+    #     print(f"Subsetted the data to the top {ngenes} highly variable genes")
+    # else:
+    #     print("Using all genes (no subsetting).")
     
-
     # Extract the counts matrix
     sc_matrix = adata.X
     print(r"Extracted the counts matrix")
@@ -229,27 +188,20 @@ if __name__ == '__main__':
     print(r"Unique classes:" + str(cell_types_unique))
 
     # Check if the class_lst.csv file already exists
-    class_lst_filename = f"class_lst{suffix}.csv" if suffix else "class_lst.csv"
-    class_lst_path = os.path.join(os.path.dirname(output_path), class_lst_filename)
     print("Checking for existing class_lst.csv file...")
     if os.path.exists(class_lst_path):
-        print(f"Warning: '{class_lst_path}' already exists.")
-        class_warning_printed = True
+        print(f"Error: '{class_lst_path}' already exists. Please specify a different path or remove the file.")
+        exit(1)
     else:
-        class_warning_printed = False
-
-    # save the unique list of classes as a csv file
-    print("Saving unique class list to CSV...")
-    numpy.savetxt(class_lst_path, 
-                  cell_types_unique, 
-                  fmt='%s',  
-                  delimiter=",")
-    if class_warning_printed:
-        print(f"Warning: '{class_lst_path}' was overwritten.")
-    else:
+        # save the unique list of classes as a csv file
+        print("Saving unique class list to CSV...")
+        numpy.savetxt(class_lst_path, 
+                    cell_types_unique, 
+                    fmt='%s',  
+                    delimiter=",")
         print(f"Unique classes saved to {class_lst_path}")
 
     # split matrix and save as individual files
     print("Splitting matrix and saving as individual files...")
-    matrix_split_save(sc_matrix, cell_types, output_path)
+    matrix_split_save(sc_matrix, cell_types, input_arrays_path)
     print("Done.")
